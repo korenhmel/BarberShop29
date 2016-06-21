6dc788da812a157b9a98d156ce13d012fa90a98f
Koren <koren.hmel@gmail.com>
2016-06-21 19:49:31 +0300

    Add Barbers migration

---
+++ db/migrate/20160621160802_create_barbers.rb
@@ -0,0 +1,11 @@
+class CreateBarbers < ActiveRecord::Migration
+  def change
+    create_table :barbers do |t|
+      t.text   :name
+      t.timestamps
+    end
+    Barber.create name: "Jessey Pinkman"
+    Barber.create name: "Walter White"
+    Barber.create name: "Gus Fring"
+  end
+end
